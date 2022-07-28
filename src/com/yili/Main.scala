0be196742a08bd067be51920252c346c998ea8ff
xiaoleilei666 <3064874795@qq.com>
2022-07-28 22:06:52 +0800

    Initial commit

---
+++ src/com/yili/Main.scala
@@ -0,0 +1,13 @@
+package com.yili
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    //print("Hello World")
+    var name = "jianlin"
+    var age = 26
+    println(name + " " + age)
+    printf("name = %s age = %d\n", name, age)
+
+  }
+
+}
